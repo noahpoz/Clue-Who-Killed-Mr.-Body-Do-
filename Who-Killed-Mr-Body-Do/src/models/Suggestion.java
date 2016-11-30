5fa85a68d0381bcc247f59a29025f86cecc7a23d
Jared Vargason <jaredvargason@gmail.com>
2016-11-30 15:37:33 -0500

    lots of model

--- Who-Killed-Mr-Body-Do/src/models/Suggestion.java
+++ Who-Killed-Mr-Body-Do/src/models/Suggestion.java
@@ -2,9 +2,9 @@ package models;
 
 public class Suggestion {
 	
-	String _person;
-	String _weapon;
-	String _room;
+	private String _person;
+	private String _weapon;
+	private String _room;
 	
 	public Suggestion(String person, String weapon, String room) {
 		_person = person;
@@ -12,5 +12,13 @@ public class Suggestion {
 		_room = room;
 	}
 	
+	public boolean equals(Suggestion s) {
+		return (s.getPerson().equals(_person) 
+				&& s.getWeapon().equals(_weapon)
+				&& s.getRoom().equals(_room));
+	}
 	
+	public String getPerson() { return _person;	}
+	public String getWeapon() { return _weapon; }
+	public String getRoom() { return _room; }
 }
